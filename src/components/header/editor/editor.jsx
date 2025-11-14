1e9f39f7f585d17a73a2b177801ab7bfb5b8413a
abdullah78-6 <abdullahqidwai92@gmail.com>
2025-11-14 15:38:28 +0530

    CODE LANGUAGE LOGIC

--- src/components/header/editor/editor.jsx
+++ src/components/header/editor/editor.jsx
@@ -1,38 +1,40 @@
 import { useContext } from "react";
 import { Helper } from "../../../store/imp";
 import { FaPlay } from "react-icons/fa";
-function Editor(){
-    const{Coderun,setcodedetails,setlang}=useContext(Helper);
+import Editor from "@monaco-editor/react"
+function Editors(){
+    const{Coderun,setcodedetails,setlang,lang,results,codedetails}=useContext(Helper);
     return <div className="mt-5  ">
         <h1 className="ml-5 text-3xl text-center">RUN YOUR CODE HERE </h1>
-        <div className="flex gap-4 items-center justify-center">
-            <form>
-                <label className="text-2xl capitalize">select your progamming language</label>
-        <input type="text" list="language" className="border-2 border-black rounded-4xl p-2" placeholder="CHOOSE-LANGUAGE" onChange={(e)=>setlang(e.target.value)}/>
-        <datalist  id="language">
-            <option   value="javascript"/>
-             <option value="C"/>
-              <option value="C++"/>
-              
-        </datalist>
+        <div className=" flex  gap-4 items-center justify-center">
+            <div>
+                <label className="text-2xl capitalize">JAVASCRIPT CODE EDITOR</label>
+        <input type="text" list="language" className="border-2 border-black rounded-4xl p-2" placeholder="CHOOSE-LANGUAGE" onChange={(e)=>setlang(e.target.value)} value="javascript" readOnly />
+        
        
-        </form>
+        </div>
+        
        
         <button className="text-2xl mt-5" onClick={Coderun}><FaPlay/>RUN</button>
+   
         </div>
+        
         <div className="flex justify-center items-center flex-wrap">
         <div className="mt-3">
+        <Editor height="70vh" width="90vh" language={lang} defaultValue="//WRITE YOUR JAVASCRIPT CODE " value={codedetails}  onChange={(value)=>setcodedetails(value)} />
             
             
-            <textarea placeholder="WRITE YOUR CODE" className="border-2 border-black w-150 h-160 text-white text-3xl ml-3 overflow-y-scroll overflow-visible" onChange={(e)=>setcodedetails(e.target.value)} >
-           </textarea>
+        
           
 
         </div>
         <div>
             
-            <textarea placeholder="CODE-OUTPUT" className="border-2 border-black w-150 h-160 text-white text-3xl ml-3 overflow-y-scroll overflow-visible" onChange={(e)=>setcodedetails(e.target.value)} readOnly>
-           </textarea>
+            <textarea placeholder="CODE-OUTPUT" className="border-2 border-black w-150 h-160 text-white text-3xl ml-3 overflow-y-scroll overflow-visible"  readOnly value={results} >
+
+            </textarea>
+            
+           
 
 
 
@@ -43,4 +45,4 @@ function Editor(){
     </div>
 
 }
-export default Editor;
+export default Editors;
--- src/store/imp.jsx
+++ src/store/imp.jsx
@@ -1,18 +1,49 @@
 import { createContext, useState } from "react";
 
+
 export const Helper=createContext();
 export function Gives({children}){
-    let[lang,setlang]=useState("empty");
+    let[lang,setlang]=useState("javascript");
     let[theme,settheme]=useState(false);
     let[codedetails,setcodedetails]=useState();
+    let [results,setresult]=useState();
    function Coderun(){
     console.log(lang);
         if(!codedetails){
             alert("PLEASE WRITE SOME CODE ");
+            return;
         }
-        console.log(codedetails);
+        if(lang.toLowerCase()==="javascript"){
+            try{
+                let output="";
+                const storelogs=(...args)=>{
+                    output+=args.join("")+"\n";
+
+                }
+                console.log=storelogs;
+                const result=eval(codedetails);
+                if(result!=undefined){
+                    output+=result;
+                }
+                setresult(output);
+
+
+            }
+            catch(err){
+                console.log(err);
+                alert("this is a ",err);
+                setresult(err);
+
+            }
+        }
+        else{
+            setresult("NOT SUPPORTED LANGUAGE");
+        }
+        console.log("this is details of our code ",codedetails);
+        
+
     }
-    return <Helper.Provider value={{lang,setlang,theme,settheme,Coderun,setcodedetails}}>
+    return <Helper.Provider value={{lang,setlang,theme,settheme,Coderun,setcodedetails,results,setresult,codedetails}}>
         {children}
     </Helper.Provider>
 }
